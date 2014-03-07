597157f5d4ba769779cd86caad9f9fb231f5f6b8
flaviohenriquealmeida <flaviohenriquealmeida@gmail.com>
2014-03-07 11:33:13 -0300

    removing unused dependencies

--- server.js
+++ server.js
@@ -10,21 +10,6 @@ var path = require('path');
 
 var app = express();
 
-// http://blog.modulus.io/nodejs-and-express-basic-authentication
-// criando sessão do usuário (cuidado, tem que vir antes. Se vier depois dá problema)
-app.use(express.cookieParser());
-app.use(express.session({secret: "This is a secret"}));
-
-// Authenticação simples
-//app.use(express.basicAuth('teste','teste'));
-/*
-// Autenticação mais flexível
-app.use(express.basicAuth(function(user, pass, callback) {
- var result = (user === 'testUser' && pass === 'testPass');
- callback(null, result);
-}));
-*/
-
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'app/views'));
@@ -54,13 +39,3 @@ load('models',  {cwd: 'app'})
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
-
-/*
-function requireAuthentication() {
-	console.log('chamou');
-}
-
-function loadUser() {
-	console.log('chamou');
-}
-*/
